b39239ae68b62a43e9710ca0e48fbebb9f56f9c7
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-02-25 12:33:11 -0500

    temp

--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,23 @@ def reload
   load 'config/environment.rb'
 end
 
-binding.pry
+dave = CarOwner.new('Dave')
+michelle = CarOwner.new('Michelle')
+
+p 'All Car Owners:'
+	p CarOwner.all
+	p "\n"
+
+p 'All Cars belonging to Dave:'
+	p dave.cars
+	# p "\n"
+p 'All Cars belonging to Michelle:'
+	p michelle.cars
+	p "\n"
+
+p 'All mechanics Dave uses:'
+	p dave.cars
+	# p "\n"
+p 'All mechanics Michelle uses:'
+	p michelle.cars
+	p "\n"
